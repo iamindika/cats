ef414b51f455ecaecdcb288fba936529706ff7af
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-06 15:38:48 +0000

    Add mocha & chai test cases for asyncBreeds.js
    Test cases moved to tester file asyncBreedsTest.js and breedDetailsFromFile was exported. Tester callback function in asyncBreeds.js was removed.

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = (breed, outputBreedDetails) =>  {
-  console.log('breedDetailsFromFile: Calling readFile...');
+//  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data:");
+//    console.log("In readFile's Callback: it has the data:");
     
     if (!error) outputBreedDetails(data);
+    else outputBreedDetails(undefined);
   });
   
 };
 
-const printOutCatBreed = breed => {
-  console.log(`Return Value: ${breed}\n`);
-};
-
-breedDetailsFromFile('Bombay', printOutCatBreed);
-breedDetailsFromFile('Balinese', printOutCatBreed);
+module.exports = breedDetailsFromFile;
